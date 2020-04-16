73ed8020a844401ac24168d2057022e5352d1498
Alexoid1 <alex_zam@hotmail.es>
2020-04-15 22:36:15 -0500

    Updates

---
+++ precache-manifest.75049a1e32371e12d270596affed2784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e4d5b0ddc78da6d855cdcef6aef3ee",
+    "url": "/Lyrics-Finder/index.html"
+  },
+  {
+    "revision": "d9e209437c81459ec63f",
+    "url": "/Lyrics-Finder/static/css/main.89bad882.chunk.css"
+  },
+  {
+    "revision": "3f26ca0a1571686a0459",
+    "url": "/Lyrics-Finder/static/js/2.57df2817.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Lyrics-Finder/static/js/2.57df2817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e209437c81459ec63f",
+    "url": "/Lyrics-Finder/static/js/main.779329b0.chunk.js"
+  },
+  {
+    "revision": "21836e3d14701eef1492",
+    "url": "/Lyrics-Finder/static/js/runtime-main.3e96dedc.js"
+  }
+]);
